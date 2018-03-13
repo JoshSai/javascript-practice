93c38f7f9c78166956294af3331860c7de48cca0
Josh Seyda <josh.seyda@gmail.com>
2018-03-13 11:34:34 -0400

    still struggling with transitions

--- section-transitions-workshop/main.js
+++ section-transitions-workshop/main.js
@@ -1,18 +1,19 @@
-
 window.addEventListener('scroll', function(){
-	let hidden1 = document.getElementsByClassName("hidden1"),
-	document.getElementsByClassName("hidden2"),
-	document.getElementsByClassName("hidden3");
+	let hidden1 = document.getElementsByClassName("hidden1");
+	let hidden2 = document.getElementsByClassName("hidden2"); 
+	let hidden3 = document.getElementsByClassName("hidden3");
 
-		 if (window.pageYOffset % 800 == 0) {
-   	 		hidden1.className += 'show';
-   	 		console.log(hidden1);
-		} else if (window.pageYOffset % 1600 == 0){
-   	 		hidden2.className += 'show';
-   	 		console.log(hidden2);
-		}else if (window.pageYOffset % 2400 == 0){
-   	 		hidden3.className += 'show';
-   	 		console.log(hidden3);
+		 if (window.pageYOffset > 1054 && window.pageYOffset < 1858) {
+   	 		hidden1.className += "show";
+   	 		//console.log(hidden1);
+		} else if (window.pageYOffset > 1858 && window.pageYOffset < 2658){
+   	 		hidden2.className += "show";
+   	 		//console.log(hidden2);
+		}else if (window.pageYOffset > 2658){
+   	 		hidden3.className += "show";
+   	 		//console.log(hidden3);
+		} else {
+			//console.log(document.getElementsByClassName('show'));
 		}
 });
 
