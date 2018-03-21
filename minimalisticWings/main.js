21ed10ed298afffe41ed0441de2068227de88cad
Josh Seyda <josh.seyda@gmail.com>
2018-03-20 22:30:55 -0400

    first API

--- minimalisticWings/main.js
+++ minimalisticWings/main.js
@@ -1,8 +1,17 @@
 $.ajax({
 	url: "https://www.redbullshopus.com/products.json" , 
 	type: 'GET' , 
-	success: function(data){
-		
+	success: function(products){
+		for (let i = 0; i < products.length; i++) {
+			let properName = products[i].title;
+			console.log(properName);
+			let mainInfo = products[i].body;
+			console.log(mainInfo);
+			let refTags = products[i].tags;
+			console.log(refTags);
+			let photoFile = products[i].variants[0].featured_image.src;
+			console.log(photoFile);
+		}
 	},
 	error: function(error){
 		console.log(error);
